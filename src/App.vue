02b898e851d157aeb96ea8cb2a2bf1e83301c3fd
iallai <iallai.info@gmail.com>
2016-05-14 14:33:50 +0800

    add router view

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,25 @@
 <template>
   <div id="app">
     <img class="logo" src="./assets/logo.png">
-    <hello></hello>
+    <router-view></router-view>
+
     <p>
-      Welcome to your Vue.js app!
+      Welcome to QingEdu
     </p>
-    <p>
-      To get a better understanding of how this boilerplate works, check out
-      <a href="http://vuejs-templates.github.io/webpack" target="_blank">its documentation</a>.
-      It is also recommended to go through the docs for
-      <a href="http://webpack.github.io/" target="_blank">Webpack</a> and
-      <a href="http://vuejs.github.io/vue-loader/" target="_blank">vue-loader</a>.
-      If you have any issues with the setup, please file an issue at this boilerplate's
-      <a href="https://github.com/vuejs-templates/webpack" target="_blank">repository</a>.
-    </p>
-    <p>
-      You may also want to checkout
-      <a href="https://github.com/vuejs/vue-router/" target="_blank">vue-router</a> for routing and
-      <a href="https://github.com/vuejs/vuex/" target="_blank">vuex</a> for state management.
-    </p>
+
+
+    <ul>
+      <li>
+        <a v-link="{ path: '/' }">Go to home</a>
+      </li>
+      <li>
+        <a v-link="{ path: '/hello' }">Go to hello</a>
+      </li>
+    </ul>
+
+
+
+
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App'
-
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
+import VueRouter from 'vue-router'
+import Hello from './components/Hello.vue'
+import Home from './components/Home.vue'
+Vue.use(VueRouter)
+let router = new VueRouter()
+router.map({
+  '/': {
+    component: Home
+  },
+  '/hello': {
+    component: Hello
+  }
 })
+router.start(App, '#admin')
