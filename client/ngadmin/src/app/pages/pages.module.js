282e868f0ab7e4b0cded9d3fb7ad7bd23afd59fe
iallai <iallai.info@gmail.com>
2016-05-16 23:50:48 +0800

    add a test module

--- client/ngadmin/src/app/pages/pages.module.js
+++ client/ngadmin/src/app/pages/pages.module.js
@@ -8,14 +8,15 @@
   angular.module('BlurAdmin.pages', [
     'ui.router',
 
-    'BlurAdmin.pages.dashboard',
-    'BlurAdmin.pages.ui',
-    'BlurAdmin.pages.components',
-    'BlurAdmin.pages.form',
-    'BlurAdmin.pages.tables',
-    'BlurAdmin.pages.charts',
-    'BlurAdmin.pages.maps',
-    'BlurAdmin.pages.profile',
+    'BlurAdmin.pages.test',
+    //'BlurAdmin.pages.dashboard',
+    //'BlurAdmin.pages.ui',
+    //'BlurAdmin.pages.components',
+    //'BlurAdmin.pages.form',
+    //'BlurAdmin.pages.tables',
+    //'BlurAdmin.pages.charts',
+    //'BlurAdmin.pages.maps',
+    //'BlurAdmin.pages.profile',
   ])
       .config(routeConfig);
 
